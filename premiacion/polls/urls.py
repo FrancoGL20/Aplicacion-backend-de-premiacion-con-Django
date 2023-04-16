d7d55971b4c76870df08791ebcabc69a39cbd5e7
FranciscoGL <francgutierrezlopez@gmail.com>
2023-04-15 18:53:53 -0600

    Creación de vistas base de app polls

--- premiacion/polls/urls.py
+++ premiacion/polls/urls.py
@@ -7,5 +7,16 @@ from . import views
 # Lista de rutas de la aplicación polls
 urlpatterns = [
     # Ruta que llama a la función index del módulo views.py
-    path("", views.index, name="index")
+    
+    # ex: /polls/ (donde polls es el nombre de la aplicación)
+    path("", views.index, name="index"), # La ruta "" es la ruta raíz de la aplicación
+    
+    # ex: /polls/5/ (donde 5 es el valor de question_id)
+    path("<int:question_id>/", views.detail, name="detail"),
+    
+    # ex: /polls/5/results/ (donde 5 es el valor de question_id)
+    path("<int:question_id>/results/", views.results, name="results"),
+    
+    # ex: /polls/5/vote/ (donde 5 es el valor de question_id)
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- premiacion/polls/views.py
+++ premiacion/polls/views.py
@@ -3,6 +3,20 @@ from django.shortcuts import render
 # Importa la función HttpResponse para poder devolver una respuesta HTTP
 from django.http import HttpResponse
 
+
 # Vista que se ejecuta cuando se llama a la ruta polls/
-def index(request):
-    return HttpResponse("Hello world. You're at the polls index.")
+def index(request): # request es un objeto que contiene información sobre la petición HTTP
+    return HttpResponse("Estás en la página principal de la aplicación polls.")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Estás viendo la pregunta número {question_id}.")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Estás viendo los resultados de la pregunta número {question_id}.")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Estás votando por la pregunta número {question_id}.")
+
